51df8cfe0279005fb97f53fb9c1671a24b4a5851
DOLOMAIN <70888845+NoneNamedNamer@users.noreply.github.com>
2024-10-31 03:37:57 +0300

    Editing comments in the button_light project

--- button_light/ae.ino
+++ button_light/ae.ino
@@ -1,23 +1,23 @@
 #include <Ds1302.h>
 
-const int axisX = A0; // ось X подключена к A0
-const int axisY = A1; // ось Y подключена к A1
-const int buttonPin = 7; // кнопка джойстика подключена к D7
-int valX, valY, valButton = 0; // переменные для хранения значений осей и кнопки
+const int axisX = A0; // axis X connected to A0
+const int axisY = A1; // axis Y connected to A1
+const int buttonPin = 7; // the joystick button is connected to D7
+int valX, valY, valButton = 0; //variables for storing axis and button values
 
 void setup() {
-pinMode(buttonPin, INPUT_PULLUP); // конфигурируем D7 как INPUT с включением
-//подтягивающего резистора
+pinMode(buttonPin, INPUT_PULLUP); // configuring D7 as an INPUT with the power on
+// the pull-up resistor
 Serial.begin(9600);
 }
 
 void loop() {
-valX = analogRead(axisX); // значение оси X
-valY = analogRead(axisY); // значение оси Y
-valButton = 1 - digitalRead(buttonPin); // значение кнопки (инвертированное)
-// выводим значения в монитор порта
+valX = analogRead(axisX); // the value of the X-axis
+valY = analogRead(axisY); // the value of the Y-axis
+valButton = 1 - digitalRead(buttonPin); // button value (inverted)
+// output the values to the port monitor
 Serial.print("X:"); Serial.print(valX, DEC);
 Serial.print(" | Y:"); Serial.print(valY, DEC);
 Serial.print(" | Button: "); Serial.println(valButton, DEC);
-delay(500); // пауза перед следующим считыванием данных
+delay(500); // pause before the next data reading
 }
